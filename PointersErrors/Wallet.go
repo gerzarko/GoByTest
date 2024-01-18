80b25c57e8290056c58784e2d54f0b489b9972ef
germanzarkovich <germanzarkovich@gmail.com>
2024-01-18 17:07:04 -0500

    add errors for testing

---
+++ PointersErrors/Wallet.go
@@ -0,0 +1,57 @@
+package PointersErrors
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface{
+    String() string
+}
+
+func (b Bitcoin) String() string{
+
+    return fmt.Sprintf("%d BTC",b)
+
+}
+
+
+type Wallet struct {
+    balance Bitcoin
+
+} 
+
+func (w *Wallet) Deposit(deposit Bitcoin){
+
+    w.balance +=  deposit 
+
+}
+
+func (w *Wallet) Balance()Bitcoin{
+
+    return w.balance
+}
+func (w *Wallet) Withdraw(amount Bitcoin)error{
+
+    if w.balance < amount {
+        return errors.New("cant withdraw that much")
+    }
+        w.balance -= amount
+        return nil
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
